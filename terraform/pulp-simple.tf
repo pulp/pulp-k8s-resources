059ea50849540ce7e5cdc3928fd1242166837ed7
git-hyagi <45576767+git-hyagi@users.noreply.github.com>
2024-07-04 14:15:17 -0300

    Add terraform configs

---
+++ terraform/pulp-simple.tf
@@ -0,0 +1,31 @@
+resource "kubernetes_manifest" "pulp_pulp" {
+  count = var.deploy_simple ? 1 : 0
+  depends_on = [ kubernetes_manifest.roles ]
+  manifest = {
+    "apiVersion" = "repo-manager.pulpproject.org/v1beta2"
+    "kind" = "Pulp"
+    "metadata" = {
+      "name" = "pulp"
+      "namespace" = var.namespace
+    }
+    "spec" = {
+      "api" = {
+        "replicas" = 1
+      }
+      "content" = {
+        "replicas" = 1
+      }
+      "web" = {
+        "replicas" = 1
+      }
+      "worker" = {
+        "replicas" = 1
+      }
+      #"pulp_settings" = {
+      #  "TELEMETRY" = false
+      #  "ANALYTICS" = false
+      #}
+      "custom_pulp_settings" = "settings"
+    }
+  }
+}
